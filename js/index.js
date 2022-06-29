4cc7e0044e2be4de5aeb306eac2682c0013e63fa
mima ch <mima.cherfa19@hotmail.fr>
2022-06-29 18:12:53 +0200

    implémentation des cards

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
 import {recipes} from "./recipes.js";
 console.log(recipes);
-const main = document.getElementById("recettes");
+const main = document.getElementById("card-grid");
 for (let index = 0; index < recipes.length; index++) {
-    const paragraph= document.createElement("h2");
-     paragraph.textContent = recipes[index].name;
- main.appendChild(paragraph);   
-    
+    const divCard = document.createElement("div")
+    divCard.classList.add("card")
+    const img = document.createElement("div")
+    img.classList.add("card_image")
+    const divTitle = document.createElement("div")
+    divTitle.classList.add("card_title_time")
+    const title= document.createElement("h1");
+     title.textContent = recipes[index].name;
+     title.classList.add("card-title")
+     const time = document.createElement("h2");
+     time.textContent = recipes[index].time+" " +"min";
+     time.classList.add("card-time")
+     const ingredients = document.createElement("div");
+    //  for (let index = 0; index < recipes.ingredients.length; index++) {
+    //     recipes.ingredients[index];  
+    ingredients.classList.add("card-ingredients")
+     const ingredient = document.createElement("h3") 
+
+    //  ingredient.textContent = recipes[index];
+     const description = document.createElement("span");
+     description.textContent = recipes[index].description;
+     description.classList.add("card-description")
+
+main.appendChild(divCard);
+ divCard.appendChild(img);
+ divCard.appendChild(divTitle); 
+ divTitle.appendChild(title);   
+ divTitle.appendChild(time)
+ divCard.appendChild(ingredients);
+ divCard.appendChild(description);
+
 }
