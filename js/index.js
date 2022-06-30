47db5dc02a25ec342d074ad1e343805afa70faa7
mima ch <mima.cherfa19@hotmail.fr>
2022-06-30 16:27:59 +0200

    affichage des cards. pbm ingredients

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 import {recipes} from "./recipes.js";
 console.log(recipes);
+
 const main = document.getElementById("card-grid");
 for (let index = 0; index < recipes.length; index++) {
+    const ingredientsTb = recipes[index].ingredients;
+
+    console.log(ingredientsTb);
     const divCard = document.createElement("div")
     divCard.classList.add("card")
     const img = document.createElement("div")
@@ -11,26 +15,37 @@ for (let index = 0; index < recipes.length; index++) {
     const title= document.createElement("h1");
      title.textContent = recipes[index].name;
      title.classList.add("card-title")
-     const time = document.createElement("h2");
+     const divTime= document.createElement("div");
+     divTime.classList.add("div-time");
+     const iconTime= document.createElement("img");
+     iconTime.classList.add("icon-time");
+     iconTime.setAttribute("src","icon-time.png");
+     const time = document.createElement("h2")
      time.textContent = recipes[index].time+" " +"min";
      time.classList.add("card-time")
-     const ingredients = document.createElement("div");
-    //  for (let index = 0; index < recipes.ingredients.length; index++) {
-    //     recipes.ingredients[index];  
+     
+    //  recipes.forEach(ingredient=> {
+        
+    
+    const ingredients = document.createElement("div");
     ingredients.classList.add("card-ingredients")
-     const ingredient = document.createElement("h3") 
-
+     const ingredt = document.createElement("h3")
+     ingredt.textContent= ingredientsTb.ingredient; 
+     
     //  ingredient.textContent = recipes[index];
      const description = document.createElement("span");
      description.textContent = recipes[index].description;
      description.classList.add("card-description")
-
+    // });
 main.appendChild(divCard);
  divCard.appendChild(img);
  divCard.appendChild(divTitle); 
- divTitle.appendChild(title);   
- divTitle.appendChild(time)
- divCard.appendChild(ingredients);
+ divTitle.appendChild(title);
+ divTitle.appendChild(divTime)
+ divTime.appendChild(iconTime);  
+ divTime.appendChild(time);
+ divCard.appendChild(ingredt);
  divCard.appendChild(description);
-
+     
+ 
 }
